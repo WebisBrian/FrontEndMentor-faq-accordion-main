7017dddba1f3f957892ea71fbaad362767b64162
WebisBrian <brian.t.github@gmail.com>
2024-11-04 13:42:13 +0100

    add Readme

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,22 +1,15 @@
 /* VARIABLES */
 
 const faq1WhatIs = document.getElementById("faq1-what-is");
-// console.dir(faq1WhatIs);
 const faq2IsFree = document.getElementById("faq2-is-free");
-// console.dir(faq2IsFree);
 const faq3ForPortfolio = document.getElementById("faq3-for-portfolio");
-// console.dir(faq3ForPortfolio);
 const faq4ImStuck = document.getElementById("faq4-im-stuck");
-// console.dir(faq4ImStuck);
 
 const faq1TextContent = document.querySelector(".faq1-text-content");
-// console.dir(faq1TextContent);
 const faq2TextContent = document.querySelector(".faq2-text-content");
-// console.dir(faq2TextContent);
 const faq3TextContent = document.querySelector(".faq3-text-content");
-// console.dir(faq3TextContent);
 const faq4TextContent = document.querySelector(".faq4-text-content");
-// console.dir(faq4TextContent);
+
 
 const plusIconSrc = "./assets/images/icon-plus.svg";
 const minusIconSrc = "./assets/images/icon-minus.svg";
